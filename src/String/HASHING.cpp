6660f07c5dd6b4eb321c02f2d5fd7a2277bf8533
nahuepera5 <n.peraza04@gmail.com>
2025-01-15 15:47:51 -0300

    add Polynomial hash with decreasing exponents.

--- src/String/HASHING.cpp
+++ src/String/HASHING.cpp
@@ -1,35 +1,30 @@
-#include <bits/stdc++.h>
-#define ll long long
-
-using namespace std;
-
-// este struct permite crear unordered_set de pares
-struct pair_hash {
-  inline std::size_t operator()(const std::pair<ll,ll> & v) const {
-    return v.first*31+v.second;
-  }
+struct StrHash { // Hash polinomial con exponentes decrecientes.
+	static constexpr ll ms[] = {1'000'000'007, 1'000'000'403};
+	static constexpr ll b = 500'000'000;
+	vector<ll> hs[2], bs[2];
+	StrHash(string const& s) {
+		int n = sz(s);
+		forn(k, 2) {
+			hs[k].resize(n+1), bs[k].resize(n+1, 1);
+			forn(i, n) {
+				hs[k][i+1] = (hs[k][i] * b + s[i]) % ms[k];
+				bs[k][i+1] =  bs[k][i] * b         % ms[k];
+			}
+		}
+	}
+	ll get(int idx, int len) const { // Hashes en `s[idx, idx+len)`.
+		ll h[2];
+		forn(k, 2) {
+			h[k] = hs[k][idx+len] - hs[k][idx] * bs[k][len] % ms[k];
+			if (h[k] < 0) h[k] += ms[k];
+		}
+		return (h[0] << 32) | h[1];
+	}
 };
 
-const int p = 31; //representa la potencia, inicializalo en base a la cantidad de letras que use el alfabeto
-const int m = 1e9 + 9; //mod
+pll union_hash(pll l, pll r, ll len_r){ //pll = pair<ll,ll>
+	l.first = ((l.first * binpow(b, len_r, ms[0])) % ms[0] + r.first) % ms[0];
+	l.second = ((l.second * binpow(b, len_r, ms[1])) % ms[1] + r.second) % ms[1];
 
-vector<ll> precompute_pow(ll size){ //el size debe ser el largo del string evaluado
-  vector<ll> res(size);
-  res[0] = 1;
-
-  for(int i = 1; i < size; i++)
-    res[i] = (res[i - 1] * p) % m;
-
-  return res;
+	return l;
 }
-
-vector<ll> precompute_hash(string s, vector<ll> pow){
-  ll size = s.size();
-  vector<ll> hs(size + 1, 0);
-
-  for(int i = 0; i < size; i++)
-    hs[i+1] = (hs[i] * ((s[i] - 'a' + 1) * pow[i]) % m) % m;
-
-  return hs;
-}
-
