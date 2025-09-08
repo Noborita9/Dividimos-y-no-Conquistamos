a1667a9adea36d50334f25391fe237ca62fd57a2
noborita <joaquinbonora09@gmail.com>
2025-09-08 00:32:57 -0300

    Upd

--- src/DataStructure/BIT_2d.cpp
+++ src/DataStructure/BIT_2d.cpp
@@ -15,7 +15,5 @@ struct BIT { // 1-Index
             tot += B[x][y];
         return tot;
     }
-    int getSum(int x1, int y1, int x2, int y2) {
-        return sum(x2, y2) - sum(x2, y1) - sum(x1, y2) + sum(x1-1,y1-1);
-    }
+    int getSum(int x1, int y1, int x2, int y2) {return sum(x2, y2) - sum(x2, y1) - sum(x1, y2) + sum(x1-1,y1-1);}
 };
