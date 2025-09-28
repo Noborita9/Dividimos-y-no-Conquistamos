8ced5da2f3375273622e49aa693baadf1d143685
Joaquin Bonora <92407663+Noborita9@users.noreply.github.com>
2025-09-28 16:24:39 -0300

    Update KMP.cpp

--- src/String/KMP.cpp
+++ src/String/KMP.cpp
@@ -1,30 +1,10 @@
-vec<int> kmp(string pat, string sec){ //geeks4geeks implementation with some changes
-  int m = pat.length();
-  int n = sec.length();
-  cout << m << " " << n << endl; 
-
-  vec<int> lps = getLps(pat);
-  vec<int> res;
-
-  int i = 0;
-  int j = 0;
-
-  while((n - i) >= (m - j)){
-    if(pat[j] == sec[i]){
-      i++;
-      j++;
+vec<int> KMP(const string &s){
+    int n = SZ(s); vec<int> pi(n);
+    L(i,1,n){
+        int j = pi[i - 1];
+        while(j>0&&s[i]!=s[j]) j = pi[j-1];
+        if (s[i]==s[j])j++;
+        pi[i]=j;
     }
-    if(j == m){
-      res.push_back(i - j);
-      j = lps[j - 1];
-    }
-    else{
-      if(i < n && pat[j] != sec[i]){
-        if(j != 0) j = lps[ j - 1 ];
-        else i = i + 1;
-      }
-    }
-  }
-
-  return res;
+    return pi;
 }
