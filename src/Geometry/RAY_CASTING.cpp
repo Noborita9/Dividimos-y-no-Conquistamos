c59ce07d7b0739e7966190dc7ba75bdbf299559a
noborita <joaquinbonora09@gmail.com>
2025-11-25 17:14:20 -0300

    Add more geo

--- src/Geometry/RAY_CASTING.cpp
+++ src/Geometry/RAY_CASTING.cpp
@@ -1,13 +1,14 @@
-typedef pair<ll, ll> Point;
-bool is_point_in_polygon(const vector<Point>& polygon, Point p) {
-    bool inside = false;
-    int n = polygon.size();
-    for (int i = 0, j = n - 1; i < n; j = i++) {
-        if ((polygon[i].second > p.second) != (polygon[j].second > p.second) &&
-            p.first < (polygon[j].first - polygon[i].first) * (p.second - polygon[i].second) / 
-                      (polygon[j].second - polygon[i].second) + polygon[i].first) {
-            inside = !inside;
+int inPolygon(const vector<pt>& p, pt a) { // 0: Outside, 1: Inside, 2: Boundary
+    int ans = 0; int n = SZ(p); 
+    L(i,0,n) {
+        pt p1 = p[i], p2 = p[(i + 1) % n];
+        if ((p2 - p1) % (a - p1) == 0 && 
+            min(p1.x, p2.x) <= a.x && a.x <= max(p1.x, p2.x) &&
+            min(p1.y, p2.y) <= a.y && a.y <= max(p1.y, p2.y)) return 2;
+        if ((p1.y > a.y) != (p2.y > a.y)) {
+            ll cp = (p2 - p1) % (a - p1);
+            if (p1.y < p2.y ? cp > 0 : cp < 0) ans = 1 - ans;
         }
     }
-    return inside;
+    return ans;
 }
