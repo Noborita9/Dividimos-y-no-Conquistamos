9724da021633d197f8194787444204eea242bd9a
noborita <joaquinbonora09@gmail.com>
2026-02-24 22:11:11 -0300

    Add Arborescence

--- src/DataStructure/BIT_2d.cpp
+++ src/DataStructure/BIT_2d.cpp
@@ -4,13 +4,13 @@ struct BIT { // 1-Index
     int n; // BUILD: N * N * log(N) * log(N)
     BIT(int n_ = 1): B(n_+1,vec<int>(n_+1)), sz(n_) {}
     void add(int i, int j, int delta){ // log(N) * log(N)
-        for (int x = i; x <= n; x += LSO(x))
-        for (int y = j; y <= n; y += LSO(y))
+        for(int x = i; x <= n; x += LSO(x))
+        for(int y = j; y <= n; y += LSO(y))
             B[x][y] += delta;
     }
     int sum(int i, int j){ // log(N) * log(N)
         int tot = 0;
-        for (int x = i; x > 0; x -= LSO(x))
+        for(int x = i; x > 0; x -= LSO(x))
         for(int y = j; y > 0; y -= LSO(y))
             tot += B[x][y];
         return tot;
--- src/Graph/bellman.cpp
+++ src/Graph/bellman.cpp
@@ -1,11 +1,22 @@
-struct Edge {int a, b, cost;};
-vector<Edge> edges;
-int solve(int s) // Source
-{
-    vector<int> d(n, INF);
-    d[s] = 0;
-    for (int i = 0; i < n - 1; ++i)
-        for (Edge e : edges)
-            if (d[e.a] < INF)
-                d[e.b] = min(d[e.b], d[e.a] + e.cost);
+bool spfa(int s, vec<int>& d) {
+    int n = SZ(g); // needs g graph
+    d.assign(n, INF);
+    vec<int> cnt(n, 0), inqueue(n, 0);
+    queue<int> q; q.push(s);
+    d[s] = 0; inqueue[s] = true;
+    while (SZ(q)) {
+        int u = q.front(); q.pop();
+        inqueue[u] = false;
+        for (auto [v, w] : g[u]) {
+            if (d[u] + w < d[v]) {
+                d[v] = d[u] + w;
+                if (!inqueue[v]) {
+                    q.push(v); inqueue[v] = true;
+                    cnt[v]++;
+                    if (cnt[v] > n) return false;  // negative cycle
+                }
+            }
+        }
+    }
+    return true;
 }
